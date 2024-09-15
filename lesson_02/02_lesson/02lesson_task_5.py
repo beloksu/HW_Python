d83e6a68354331870ad436e9408212e7149d515c
Ксения <Kseniia@MacBook-Pro-A.local>
2024-09-15 08:28:24 +0700

    third_try

--- lesson_02/02_lesson/02lesson_task_5.py
+++ lesson_02/02_lesson/02lesson_task_5.py
@@ -1,5 +1,5 @@
 def month_to_season(month):
-    if 1 <= month <= 2:
+    if 1 <= month <= 2 or 12 <= month <= 12:
         return "Зима"
     elif 3 <= month <= 5:
         return "Весна"
@@ -7,8 +7,5 @@ def month_to_season(month):
         return "Лето"
     elif 9 <= month <= 11:
         return "Осень"
-    elif 12 <= month <= 12:
-        return "Зима"
     else:
         return "Неверный номер месяца"
-    
